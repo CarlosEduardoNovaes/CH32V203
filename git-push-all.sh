a95febb0b79e435b8f960f668cf2884adc48d197
CarlosEduardoNovaes <carlo@DESKTOP-1D2UKND.>
2025-04-24 19:32:40 -0300

    comit

---
+++ git-push-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Faz add de todos os arquivos, commit com mensagem fornecida e push
+
+echo "Mensagem do commit:"
+mensagem="comit"
+
+git status
+git add .
+git commit -m "$mensagem"
+BRANCH=$(git branch --show-current)
+git push origin "$BRANCH"
